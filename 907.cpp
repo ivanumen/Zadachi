4d15d3a55af7d4cde9da11a26dfd69c1df6f2e14
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-13 19:23:15 +0300

    zadacha 907

---
+++ 907.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int sdvig1(int number) {
+	vector <int> element(number);
+	vector <int> sdvigElement(number);
+	for (int i = 0; i < number; i++) {
+		cin >> element[i];
+	}
+	sdvigElement[0] = element[number - 1];
+	cout << sdvigElement[0] << " ";
+	for (int i = 1; i < number; i++){
+		sdvigElement[i] = element[i - 1];
+		cout << sdvigElement[i] << " ";
+	}
+	return 0;
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	sdvig1(number);
+	return 0;
+}
+
