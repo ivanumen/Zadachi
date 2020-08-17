d0410cafe2c0b74521012532d94858f2155e8486
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-17 20:44:52 +0300

    Problem 1124

---
+++ 1124.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void alphabetArray(int number) {
+	int numberAlphabet;
+	int numberOfSpaces = number - 1;
+	for (int i = 0; i < number; i++) {
+		numberAlphabet = 0;
+		cout << "a";
+		for (int j = 0; j < number; j++) {
+			if (numberOfSpaces <= j) {
+				cout << (char)('a' + numberAlphabet);
+				++numberAlphabet;
+			}
+			else {
+				cout << " ";
+			}
+		}
+		--numberOfSpaces;
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	alphabetArray(number);
+}
