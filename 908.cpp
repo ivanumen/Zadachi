0e4d238ffc08ca5f2ca828e4415770de9851e433
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-17 13:17:43 +0300

    problem 908 (#2)

---
+++ 908.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int delenie6(int number) {
+	vector <int> positiveNumber(number);
+	int sumpositiveNumber = 0;
+	int kolpositiveNumber = 0;
+	for (int i = 0; i < number; i++) {
+		cin >> positiveNumber[i];
+		if (positiveNumber[i] > 0 && positiveNumber[i] % 6 == 0) {
+			sumpositiveNumber = sumpositiveNumber + positiveNumber[i];
+			++kolpositiveNumber;
+		}
+	}
+	cout << kolpositiveNumber << " " << sumpositiveNumber;
+	return 0;
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	delenie6(number);
+	return 0;
+}
+
