5c093017668529d448501450fe909491570fefa8
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-18 18:19:49 +0300

    problem 175642

---
+++ 175642.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int divisionWithoutRemainder(const int pupils, const int apples) {
+	return (int)(apples % pupils);
+}
+
+int main()
+{
+	int pupils, apples;
+	cin >> pupils;
+	cin >> apples;
+	cout << divisionWithoutRemainder(pupils, apples);
+	return 0;
+}
