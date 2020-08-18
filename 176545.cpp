4161e65e89fbb4147280599da8424bdc275662ef
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-18 19:33:35 +0300

    problem 175645

---
+++ 176545.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int perimeterRectangle(const int sideA, const int sideB) {
+	return sideA * sideB;
+}
+
+int main()
+{
+	int sideA, sideB;
+	cin >> sideA;
+	cin >> sideB;
+	cout << perimeterRectangle(sideA, sideB);
+}
