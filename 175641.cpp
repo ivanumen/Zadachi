986027f015b6d23818ab3d8c255fbbfe3537b4d4
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-18 19:23:04 +0300

    problem 175641

---
+++ 175641.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int numberOfDigits(long int number) {
+	int digits = 0;
+	if (number == 0) {
+		++digits;
+	}
+	while (number > 0) {
+		++digits;
+		number /= 10;
+	}
+	return digits;
+}
+
+int main()
+{
+	long int number;
+	cin >> number;
+	cout << numberOfDigits(number);
+	return 0;
+}
