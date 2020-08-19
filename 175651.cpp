626d4b4f289efa5caf4ca6ce69730b7c19aa26ec
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-19 23:34:37 +0300

    problem 175651

---
+++ 175651.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void parity(long long number) {
+	if (number % 2 >= 1) {
+		cout << "ODD";
+	}
+	else {
+		cout << "EVEN";
+	}
+}
+
+int main()
+{
+	long long number;
+	cin >> number;
+	parity(number);
+	return 0;
+}
