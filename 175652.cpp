9b5110b10beec32c47734423528bb7a4dc263093
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-19 23:10:50 +0300

    problem 175652

---
+++ 175652.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long numberNext(long long number) {
+	return --number;
+}
+
+int main()
+{
+	long long number;
+	cin >> number;
+	cout << numberNext(number);
+	return 0;
+}
