835bd51eb131a341b57e189bd9d4ca782846770e
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-19 22:45:24 +0300

    problem 175648

---
+++ 175648.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void determinant(int number) {
+	if (number > 0) {
+		cout << "Positive";
+	}
+	if (number == 0) {
+		cout << "Zero";
+	}
+	if (number < 0) {
+		cout << "Negative";
+	}
+}
+
+
+int main()
+{
+	int number;
+	cin >> number;
+	determinant(number);
+	return 0;
+}
