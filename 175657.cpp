6fcc2296fbffc0c044f5b681587ec08c45f06000
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-20 22:11:39 +0300

    problem 175657

---
+++ 175657.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long averageNumber(const long long number1, const long long number2, const long long number3) {
+	if ((number1 > number2 && number1 < number3) || (number1 > number3 && number1 < number2)) {
+		return number1;
+	}
+	if ((number2 > number1 && number2 < number3) || (number2 > number3 && number2 < number1)) {
+		return number2;
+	}
+	else {
+		return number3;
+	}
+}
+
+int main()
+{
+	long long number1, number2, number3;
+	cin >> number1;
+	cin >> number2;
+	cin >> number3;
+	cout << averageNumber(number1, number2, number3);
+	return 0;
+}
