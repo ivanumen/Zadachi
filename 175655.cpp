b0ba9983b5cbebb7f05ba687fc66fa6a97607436
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-20 21:54:05 +0300

    problem 175655

---
+++ 175655.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long sumChildren(long long boys, long long girls) {
+	return boys + girls;
+}
+
+int main()
+{
+	long long boys, girls;
+	cin >> boys;
+	cin >> girls;
+	cout << sumChildren(boys, girls);
+	return 0;
+}
