9c54b658e3162a0f277988c4d514e7133ccfb531
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-20 21:45:06 +0300

    problem 175653

---
+++ 175653.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long numberEnd(long long number) {
+	return number % 10;
+}
+
+int main()
+{
+	long long number;
+	cin >> number;
+	cout << numberEnd(number);
+	return 0;
+}
