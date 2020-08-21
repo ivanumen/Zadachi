fec69251842802c58aea0c27e85daced1324652a
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-21 17:40:46 +0300

    problem 175654

---
+++ 175654.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "cmath"
+
+using namespace std;
+
+int conditionalOperator(int number) {
+	if (number < 5) {
+		return pow(number, 2) - (3 * number) + 4;
+	}
+	if (number >= 5) {
+		return number + 7;
+	}
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	cout << conditionalOperator(number);
+	return 0;
+}
