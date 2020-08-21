c44762511ebdf67c444a38a499158a0e8f960a5b
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-21 22:24:38 +0300

    problem 175659

---
+++ 175659.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+vector <int> intToVector(int number) {
+	vector <int> decomposition;
+	int secondNumber;
+	while (number > 0) {
+		secondNumber = number % 10;
+		number /= 10;
+		decomposition.push_back(secondNumber);
+	}
+	reverse(decomposition.begin(), decomposition.end());
+	return decomposition;
+}
+
+int powSumVector(const vector <int>& in) {
+	int ans = 0;
+	for (int i = 0; i < in.size(); i++) {
+		ans += in[i];
+	}
+	return pow(ans, 2);
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	cout << powSumVector(intToVector(number));
+	return 0;
+}
