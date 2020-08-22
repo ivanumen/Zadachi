099f9877200a232b7d0c0254368420bfef493741
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-22 11:39:28 +0300

    problem 175647

---
+++ 175647.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void price(int gvn, int cop, int number) {
+	gvn *= number;
+	cop *= number;
+	if (cop >= 100) {
+		gvn += (cop / 100);
+		cop %= 100;
+	}
+	cout << gvn << " " << cop;
+}
+
+int main()
+{
+	int number, gvn, cop;
+	cin >> gvn;
+	cin >> cop;
+	cin >> number;
+	price(gvn, cop, number);
+	return 0;
+}
