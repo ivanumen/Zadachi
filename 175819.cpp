d7cb9889ebfcdf55d1928da06d4facbcba506209
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-24 21:58:24 +0300

    problem 175819

---
+++ 175819.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void triangle(const int numberA, const int numberB, const int numberC) {
+	if (numberA == numberB && numberA == numberC) {
+		cout << "1";
+	}
+	else {
+		if (numberA == numberB || numberA == numberC || numberB == numberC) {
+			cout << "2";
+		}
+		else {
+			if (numberA != numberB && numberA != numberC && numberB != numberC) {
+				cout << "3";
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	int numberA, numberB, numberC;
+	cin >> numberA;
+	cin >> numberB;
+	cin >> numberC;
+	triangle(numberA, numberB, numberC);
+	return 0;
+}
