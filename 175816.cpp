6db40a28c21452965f4db3ee237a8327f57d97a2
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-24 19:15:59 +0300

    problem 175816

---
+++ 175816.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void estimations(int number) {
+	if (number >= 1 && number <= 3) {
+		cout << "Initial";
+	}
+	if (number >= 4 && number <= 6) {
+		cout << "Average";
+	}
+	if (number >= 7 && number <= 9) {
+		cout << "Sufficient";
+	}
+	if (number >= 10 && number <= 12) {
+		cout << "High";
+	}
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	estimations(number);
+	return 0;
+}
