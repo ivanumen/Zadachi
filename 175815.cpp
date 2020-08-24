25dee40690fa73d279e06b439f945ceb71870d9a
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-24 19:06:21 +0300

    problem 175815

---
+++ 175815.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector <int> intToVector(int number) {
+	vector <int> decomposition;
+	int secondNumber;
+	while (number > 0) {
+		secondNumber = number % 10;
+		number /= 10;
+		decomposition.push_back(secondNumber);
+	}
+	reverse(decomposition.begin(), decomposition.end());
+	return decomposition;
+}
+
+int compositionVector(const vector <int>& in) {
+	int ans = 1;
+	for (int i = 0; i < in.size(); i++) {
+		ans *= in[i];
+		if (in[i] == 0) {
+			break;
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	cout << compositionVector(intToVector(number));
+	return 0;
+}
