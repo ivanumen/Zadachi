862a017cc57922fc8ac22604224b822d7879c608
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-24 20:21:02 +0300

    problem 175818

---
+++ 175818.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void season(int number) {
+	if (number == 1 || number == 2 || number == 12) {
+		cout << "Winter";
+	}
+	if (number >= 3 && number <= 5) {
+		cout << "Spring";
+	}
+	if (number >= 6 && number <= 8) {
+		cout << "Summer";
+	}
+	if (number >= 9 && number <= 11) {
+		cout << "Autumn";
+	}
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	season(number);
+	return 0;
+}
