406e4340e13cab7b47b158a91a3de9ac52464890
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-25 15:36:36 +0300

    problem 175825

---
+++ 175825.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+long long sumNumber() {
+	long long number1, number2;
+	std::cin >> number1;
+	std::cin >> number2;
+	return number1 + number2;
+}
+
+int main()
+{
+	std::cout << sumNumber();
+	return 0;
+}
