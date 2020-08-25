c26122c96b9f3c8beab869423760c4595ddd144c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-25 11:16:46 +0300

    problem 175822

---
+++ 175822.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector <int> intToVector(int number) {
+	vector <int> decomposition;
+	int secondNumber;
+	if (number < 0) {
+		number *= -1;
+	}
+	while (number > 0) {
+		secondNumber = number % 10;
+		number /= 10;
+		decomposition.push_back(secondNumber);
+	}
+	reverse(decomposition.begin(), decomposition.end());
+	return decomposition;
+}
+
+void writeVector(const vector <int>& out) {
+	for (int i = 0; i < out.size(); i++) {
+		cout << out[i] << endl;
+	}
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	writeVector(intToVector(number));
+	return 0;
+}
+
