d420bfc2e68f9a20d04f25a56c6c2b51879f1507
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-25 16:57:04 +0300

    problem 175814 (#7)

---
+++ 175814.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int friends(int number) {
+	if (number == 0) {
+		return 0;
+	}
+	return --number;
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	cout << friends(number);
+	return 0;
+}
