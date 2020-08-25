5e2bdfdf03728d28ceccb49947fa013e793cade8
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-25 16:56:01 +0300

    problem 940 (#4)

---
+++ 940.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+void readVector(vector <int>& in) {
+	int number;
+	cin >> number;
+	for (int i = 0; i < number; i++) {
+		int x;
+		cin >> x;
+		in.push_back(x);
+	}
+}
+
+int majoringElement() {
+	vector <int> in;
+	readVector(in);
+	unordered_map<int, int> majoringArray;
+	for (int i = 0; i < in.size(); i++) {
+		majoringArray[in[i]]++;
+		if (majoringArray[in[i]] > (in.size() / 2)) {
+			return in[i];
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	cout << majoringElement();
+	return 0;
+}
