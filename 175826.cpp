5380a87b01b2f992e03858a204c9e60b7a32e765
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-25 15:57:34 +0300

    problem 175826

---
+++ 175826.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int sumMaxMin(const int numberA, const int numberB, const int numberC) {
+	return max(max(numberA, numberB), max(numberA, numberC)) + min(min(numberA, numberB), min(numberA, numberC));
+}
+
+int main()
+{
+	int costA, costB, costC;
+	cin >> costA;
+	cin >> costB;
+	cin >> costC;
+	cout << sumMaxMin(costA, costB, costC);
+	return 0;
+}
