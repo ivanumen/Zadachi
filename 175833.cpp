bffb64dc3537f976c3625bca7e7443a383455d8a
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-26 20:05:12 +0300

    problem 175833

---
+++ 175833.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double operatorValue(const double number) {
+	return pow(number, 3) - (5 * pow(number, 2) / 7) + 9 * number - (3 / number) + 1;
+}
+
+int main()
+{
+	double number;
+	cin >> number;
+	printf("%.3f",operatorValue(number));
+	return 0;
+}
