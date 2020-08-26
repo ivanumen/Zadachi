c8290fe6383a317dba1786eb36ce5fcbb02ae5b2
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-26 19:53:44 +0300

    problem 175832

---
+++ 175832.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long concert(const long long numberA, const long long numberB, const long long numberC) {
+	return numberA + numberB - numberC;
+}
+
+int main()
+{
+	long long sang, danced, sangAndDansed;
+	cin >> sang;
+	cin >> danced;
+	cin >> sangAndDansed;
+	cout << concert(sang, danced, sangAndDansed);
+	return 0;
+}
