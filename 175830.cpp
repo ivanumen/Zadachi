fd7698bc27ab1152d96d2ab358b308945270e04c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-26 19:45:21 +0300

    problem 175830

---
+++ 175830.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long conditionalOperator(const int number) {
+	if (number >= 10) {
+		return pow(number, 3) + (5 * number);
+	}
+	else {
+		return pow(number, 2) - (2 * number) + 4;
+	}
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	cout << conditionalOperator(number);
+	return 0;
+}
