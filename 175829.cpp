f916475123916d402667ae28aa8d9a3f71af3d8c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-26 19:35:06 +0300

    problem 175829

---
+++ 175829.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int ceilSum(const double numberA, const double numberB, const double numberC) {
+	return ceil(numberA / numberC) + ceil(numberB / numberC);
+}
+
+int main()
+{
+	double boys, girls, rooms;
+	cin >> boys;
+	cin >> girls;
+	cin >> rooms;
+	cout << ceilSum(boys, girls, rooms);
+	return 0;
+}
