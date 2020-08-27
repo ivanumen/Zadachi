20b41bcb73172cf8c99158c6850a71503fc5ad99
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-27 20:22:45 +0300

    problem 175823

---
+++ 175823.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void divisibility(const int numberA, const int numberB) {
+	if (numberA % numberB > 1) {
+		cout << numberA / numberB << " " << numberA % numberB;
+	}
+	else {
+		cout << "Divisible";
+	}
+}
+
+int main()
+{
+	int numberA, numberB;
+	cin >> numberA;
+	cin >> numberB;
+	divisibility(numberA, numberB);
+	return 0;
+}
