97b1866d4b5fc9115cbe76261e327623f23d3303
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-28 14:15:05 +0300

    problem 176065

---
+++ 176065.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <utility>
+
+using namespace std;
+
+template <class T>
+const pair<T, T> divisoraQnduotient(const T number1, const T number2) {
+	return make_pair(number1 / number2, number1 % number2);
+}
+
+template <class T>
+void writePair(const pair<T, T>& in) {
+	cout << in.first << " " << in.second;
+}
+
+
+
+int main()
+{
+	long long number1, number2;
+	cin >> number1;
+	cin >> number2;
+	writePair(divisoraQnduotient(number1, number2));
+	return 0;
+}
+
