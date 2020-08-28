49d45d6bcf3a31e5fc0a2c7577ac9342f7bed3db
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-28 14:00:39 +0300

    problem 176066

---
+++ 176066.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <utility>
+
+using namespace std;
+
+
+void perimeterAndAreaOfAsquare() {
+	int number;
+	while (cin >> number) {
+		cout << number * 4 << " " << number * number << endl;
+	}
+}
+
+
+
+
+int main()
+{
+	perimeterAndAreaOfAsquare();
+	return 0;
+}
+
