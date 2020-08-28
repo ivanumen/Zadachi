e66c64ac9e509bedc81c456ccdb5143eb7acc1e6
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-28 13:21:04 +0300

    problem 176067

---
+++ 176067.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+long long composition(const long long number1, const long long number2) {
+	return (number1 * number2);
+}
+
+
+int main()
+{
+	long long number1, number2;
+	std::cin >> number1;
+	std::cin >> number2;
+    std::cout << composition(number1, number2);
+	return 0;
+}
+
