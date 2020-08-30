337220f26bcd80d23f16e75fce63afd8be7a0443
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-30 23:43:49 +0300

    problem 176063

---
+++ 176063.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int daysFromSeconds(const long long seconds) {
+	return seconds / 86400;
+}
+
+int hoursFromSeconds(const long long seconds) {
+	return (seconds % 86400) / 3600;
+}
+
+int minetsFromSeconds(const long long seconds) {
+	return ((seconds % 86400) % 3600) / 60;
+}
+
+
+int main()
+{
+	long long seconds;
+	cin >> seconds;
+	cout << daysFromSeconds(seconds) << " "
+		<< hoursFromSeconds(seconds) << " "
+		<< minetsFromSeconds(seconds) << " "
+		<< ((seconds % 86400) % 3600) % 60;
+	return 0;
+}
+
