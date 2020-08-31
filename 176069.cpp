462145b165616774d2c99a034fbe14b62e01bd23
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-31 15:35:53 +0300

    problem 176069

---
+++ 176069.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+
+template <class T>
+int coordinatePlane(const T x, const T y) {
+	if (x == 0 || y == 0) {
+		return 0;
+	}
+	if (x > 0 && y > 0) {
+		return 1;
+	}
+	if (x < 0 && y < 0) {
+		return 3;
+	}
+	if (x < 0 && y > 0) {
+		return 2;
+	}
+	else {
+		return 4;
+	}
+}
+
+int main()
+{
+	double x, y;
+	std::cin >> x >> y;
+    std::cout << coordinatePlane(x, y);
+}
+
