360981a7e6e3bb735acbb75bc02e9ed7727b6891
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-08-31 15:23:11 +0300

    problem 176068

---
+++ 176068.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <class T>
+int zeroInFactorial(T number) {
+	number /= 5;
+	int count = number;
+	while (count / 5 >= 1) {
+		count /= 5;
+		number += count;
+	}
+	return number;
+}
+
+int main()
+{
+	long long number;
+	cin >> number;
+	cout << zeroInFactorial(number);
+	return 0;
+}
