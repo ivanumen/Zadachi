7b5075492c83e4c790039db87aaffeeb4f787b36
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-02 22:47:11 +0300

    problem 176072

---
+++ 176072.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+template <class T>
+void readVectorUntilEOF(vector <T>& in) {
+	T x;
+	while (cin >> x) {
+		in.push_back(x);
+	}
+}
+
+template <class T>
+T maxElemVector(const vector <T>& in) {
+	T maxElem = in[0];
+	for (const auto elem : in) {
+		maxElem = max(maxElem, elem);
+	}
+	return maxElem;
+}
+
+int main()
+{
+	vector <double> in;
+	readVectorUntilEOF(in);
+	printf("%.2f", maxElemVector(in));
+}
