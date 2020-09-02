a83150d0e8a210c6ad1c8e0124bd2a6b0cde0ede
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-02 23:00:23 +0300

    problem 176073

---
+++ 176073.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int housNumbers(const int numberHous1, const int numberHous2) {
+	if ((numberHous1 % 2) == (numberHous2 % 2)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int main()
+{
+	int numberHous1, numberHous2;
+	std::cin >> numberHous1 >> numberHous2;
+	std::cout << housNumbers(numberHous1, numberHous2);
+}
+
