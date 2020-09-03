dad3c8708b25889020f1ab78c2f4e8d01bc4353d
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-03 13:34:35 +0300

    problem 176078

---
+++ 176078.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int equalToTheDivisor(const int number) {
+	int count = 0;
+	for (int i = 1; i <= number; i++) {
+		if (number % i == number / i) {
+			++count;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+	int number;
+	std::cin >> number;
+    std::cout << equalToTheDivisor(number);
+}
+
