ffff23d86dbbfde002e961857f2cc7e8ce92e8f4
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-03 13:10:32 +0300

    problem 176075

---
+++ 176075.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+long long areaCube(const long long a, const long long b, const long long c) {
+	return 2 * (a * b + b * c + a * c);
+}
+
+long long volumeCube(const long long a, const long long b, const long long c) {
+	return a * b * c;
+}
+
+int main()
+{
+	long long a, b, c;
+	std::cin >> a >> b >> c;
+    std::cout << areaCube(a, b, c) << " " << volumeCube(a, b, c);
+}
+
