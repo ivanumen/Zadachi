ff1334ff8bd00658c22ab9af2bf2a51c8a2dafc3
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-03 13:45:21 +0300

    problem 176079

---
+++ 176079.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int gardener(const int number) {
+	int foliage = 1;
+	for (int i = 1; i <= number; i++) {
+		foliage += i * 2;
+	}
+	return foliage;
+}
+
+int main()
+{
+	int number;
+	std::cin >> number;
+    std::cout << gardener(number);
+}
+
