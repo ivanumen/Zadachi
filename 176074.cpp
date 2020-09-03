ac9e70c0abee4b8e2d4c0a0c75ff30c4e2194272
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-03 12:58:30 +0300

    problem 176074

---
+++ 176074.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int transformation(int number) {
+	int count = 0;
+	while (number != 1) {
+		++count;
+		if (number % 2 < 1) {
+			number /= 2;
+		}
+		else {
+			++number;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+	int number;
+	std::cin >> number;
+    std::cout << transformation(number);
+}
