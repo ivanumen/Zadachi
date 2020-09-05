a9bbab339842f1f8e8f1a8b9b5a576ee47d8e736
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-05 16:27:15 +0300

    problem 176076

---
+++ 176076.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector <char> svap(const int number) {
+	vector <char> GCV = {'G', 'C', 'V'};
+	for (int i = 0; i < number; i++) {
+		swap(GCV[2], GCV[1]);
+		swap(GCV[1], GCV[0]);
+	}
+	return GCV;
+}
+
+
+template <class T>
+void writeVector(const vector <T>& out) {
+	for (const auto& elem : out) {
+		cout << elem;
+	}
+}
+
+int main()
+{
+	int number, iteration;
+	cin >> iteration;
+	for (int i = 0; i < iteration; i++) {
+		cin >> number;
+		writeVector(svap(number));
+		cout << endl;
+	}
+}
+
