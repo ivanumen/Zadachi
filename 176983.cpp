e2d34c91a325582ddefe23eadba8a747a381f01f
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-08 20:45:12 +0300

    problem 176983

---
+++ 176983.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int ATM(int money) {
+	int countBanknotes = 0;
+	int count = money / 500;
+	countBanknotes += count;
+	money -= count * 500;
+	count = money / 200;
+	countBanknotes += count;
+	money -= count * 200;
+	count = money / 100;
+	countBanknotes += count;
+	money -= count * 100;
+	count = money / 50;
+	countBanknotes += count;
+	money -= count * 50;
+	count = money / 20;
+	countBanknotes += count;
+	money -= count * 20;
+	count = money / 10;
+	countBanknotes += count;
+	money -= count * 10;
+	if (money > 0) {
+		return -1;
+	}
+	return countBanknotes;
+}
+
+int main()
+{	
+	int money;
+	std::cin >> money;
+    std::cout << ATM(money);
+}
+
