ec7c3c530e510c5557c8353624e0c2331dc5a810
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-10 22:48:24 +0300

    problem 176980

---
+++ 176980.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVector(vector <T>& in) {
+	int number;
+	cin >> number;
+	in.resize(number);
+	for (auto& elem : in) {
+		cin >> elem;
+	}
+}
+
+template <class T>
+T countMaxElemVector(const vector <T>& in) {
+	int count = 0;
+	T maxElem = in[1];
+	for (auto& elem : in) {
+		if (elem > maxElem) {
+			count = 0;
+		}
+		maxElem = max(maxElem, elem);
+		if (maxElem == elem) {
+			++count;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+	vector <int> in;
+	readVector(in);
+    cout << countMaxElemVector(in);
+}
+
