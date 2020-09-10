1a3ea228c8d0c80c15f60996f5f8ece4878432ff
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-10 22:30:11 +0300

    problem 176992

---
+++ 176992.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+T maxElemVector(const vector <T>& in) {
+	return *max_element(in.begin(), in.end());
+}
+
+int main()
+{
+	int elem1, elem2, elem3, elem4;
+	cin >> elem1 >> elem2 >> elem3 >> elem4;
+	vector <int> in = { elem1, elem2, elem3, elem4 };
+	cout << maxElemVector(in);
+}
+
