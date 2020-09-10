ae1a5699555b2aac0c4c0721515e61bc058aed51
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-10 22:58:53 +0300

    problem 176994

---
+++ 176994.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int conditionalOperator(const int x) {
+	int y = 0;
+	if (x < -4) {
+		y = x + 5;
+	}
+	if (x >= -4 && x <= 7) {
+		y = pow(x, 2) - (3 * x);
+	}
+	if (x > 7) {
+		y = pow(x, 3) + (2 * x);
+	}
+	return y;
+}
+
+int main()
+{
+	int x;
+	cin >> x;
+    cout << conditionalOperator(x);
+}
+
