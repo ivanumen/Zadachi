f324863641d77f09ddde7f7a0ebd062fb7dc59b9
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-11 14:06:46 +0300

    problem 176993

---
+++ 176993.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+long long numbersHotel(const long long floors, const long long rooms) {
+	return (floors - 1) * rooms * 3;
+}
+
+int main()
+{
+	long long floors, rooms;
+	std::cin >> floors >> rooms;
+    std::cout << numbersHotel(floors, rooms);
+}
