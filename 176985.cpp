1661431e716763ca1d60878003e5c6ce43d6292a
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-11 13:58:01 +0300

    problem 176985

---
+++ 176985.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+
+long long areaCube(const long long edge) {
+	return 6 * pow(edge, 2);
+}
+
+long long volumeCube(const long long edge) {
+	return pow(edge, 3);
+}
+
+
+int main()
+{
+	long long edge;
+	std::cin >> edge;
+    std::cout << areaCube(edge) << " " << volumeCube(edge);
+}
+
