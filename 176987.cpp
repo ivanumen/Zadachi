a69b881b5f950f64741978299e71295ce881ce57
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-11 15:39:49 +0300

    problem 176987

---
+++ 176987.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+void readVector(vector <T>& in) {
+	int number;
+	cin >> number;
+	in.resize(number);
+	for (auto& elem : in) {
+		cin >> elem;
+	}
+}
+
+template <class T>
+pair <int, T> sumAndCountNegativVector(const vector <T>& in) {
+	T ans = 0;
+	int count = 0;
+	for (const auto elem : in) {
+		if (elem < 0) {
+			ans += elem;
+			++count;
+		}
+	}
+	return make_pair(count, ans);
+}
+
+void writePair(const pair<int, double>& out) {
+	cout << out.first << " ";
+	printf("%.2F", out.second);
+}
+
+int main()
+{
+	vector <double> in;
+	readVector(in);
+	writePair(sumAndCountNegativVector(in));
+}
