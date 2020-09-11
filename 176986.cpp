4b336f3442001ffa5a7f29204d1d977a5b3b829c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-11 17:48:48 +0300

    problem 176986

---
+++ 176986.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+vector <int> fromDecimalToBinary(T number) {
+	vector <int> binari;
+	int binariNumber;
+	while (number > 0) {
+		binariNumber = number % 2;
+		number /= 2;
+		binari.push_back(binariNumber);
+	}
+	reverse(binari.begin(), binari.end());
+	return binari;
+}
+
+
+int negativeBus() {
+	int number, elem;
+	vector <int> in;
+	int size = 0;
+	cin >> number;
+	int count = 0;
+	for (int i = 0; i < number; i++) {
+		cin >> elem;
+		if (elem <= 1) {
+			continue;
+		}
+		in = fromDecimalToBinary(elem);
+		size = in.size() - 2;
+		if (in[size] == 1) {
+			++count;
+		}
+		in.clear();
+	}
+	return count;
+}
+
+int main()
+{
+	cout << negativeBus();
+	return 0;
+}
+
+
