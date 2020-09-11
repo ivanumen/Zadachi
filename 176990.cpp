cff56023fffc85540ec3fdd70bf57e934e601207
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-11 14:24:31 +0300

    problem 176990

---
+++ 176990.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int maxCount(const int number1, const int number2) {
+	if (number1 == number2) {
+		return 1;
+	}
+	return (number1 - number2) + 1;
+}
+
+int minCount(const double number1, const double number2) {
+	return ceil(number1 / number2);
+}
+
+int main()
+{
+	int canarys, cages;
+	std::cin >> canarys >> cages;
+    std::cout << minCount(canarys, cages) << " " << maxCount(canarys, cages);
+}
+
