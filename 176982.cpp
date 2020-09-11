d71b48a2c8182e103396a9f649ac46838b937086
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-11 13:24:03 +0300

    problem 176982

---
+++ 176982.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string divisibilityByNumbers(const int number, const int number1, const int number2){
+	if (number % number1 < 1 && number % number2 < 1) {
+		return "YES";
+	}
+	else {
+		return "NO";
+	}
+}
+
+int main()
+{
+	int number, number1, number2;
+	cin >> number >> number1 >> number2;
+    cout << divisibilityByNumbers(number, number1, number2);
+}
+
