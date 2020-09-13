a04678712dd12e5da229dcd53399d17bfcb435c9
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-13 22:19:13 +0300

    problem 177615

---
+++ 177615.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+vector <int> intToVector(T number) {
+	vector <T> decomposition;
+	int secondNumber;
+	while (number > 0) {
+		secondNumber = number % 10;
+		number /= 10;
+		decomposition.push_back(secondNumber);
+	}
+	reverse(decomposition.begin(), decomposition.end());
+	return decomposition;
+}
+
+template <class T>
+T multNoZeroVector(const vector <T>& in) {
+	T ans = 1;
+	for (const auto elem : in) {
+		if (elem != 0) {
+			ans *= elem;
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+    cout << multNoZeroVector(intToVector(number));
+	return 0;
+}
+
