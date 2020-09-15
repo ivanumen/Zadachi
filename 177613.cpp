50ed62b75abfeac4a263dc478d878ad5dbbbb7ba
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-15 14:20:51 +0300

    problem 177613

---
+++ 177613.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+template <class T>
+T  extent(const T number, const int rank) {
+	T elem = 1;
+	for (int i = 0; i < rank; i++) {
+		elem *= number;
+	}
+	return elem;
+}
+
+int main()
+{
+	float x, y;
+	std::cin >> x >> y;
+    printf("%.3f", ((2 * x + y) / (extent(x, 2) - x * y  + 4 * extent(y, 2))) + ((2 * extent(x, 2) - x * y + extent(y, 2)) / (x + 4 * y))) ;
+}
+
