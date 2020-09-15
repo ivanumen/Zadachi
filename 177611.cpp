7aa506f12219e7469c16af6812c6892876f05129
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-15 20:10:43 +0300

    problem 177611

---
+++ 177611.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+vector <int> intToVector(T number) {
+	vector <T> decomposition;
+	int secondNumber;
+	while (number > 0) {
+		secondNumber = number % 10;
+		number /= 10;
+		decomposition.push_back(secondNumber);
+	}
+	return decomposition;
+}
+
+template <class T>
+void writeVector(const vector <T>& out) {
+	for (const auto& elem : out) {
+		cout << elem;
+	}
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	writeVector(intToVector(number));
+	return 0;
+}
