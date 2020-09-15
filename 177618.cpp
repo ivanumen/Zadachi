1e6f19b51e5b822302abcce36fb93c5c3d234b49
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-15 16:54:46 +0300

    problem 177618

---
+++ 177618.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+long long countPair(long long number) {
+	long long count = 1;
+	long long prevCountPairElem = 0;
+	long long countPairElem = 0;
+	while (number > 0)
+	{
+		count *= 10;
+		countPairElem = (count  / 2) + (prevCountPairElem - 1);
+		prevCountPairElem -= countPairElem;
+		--number;
+	}
+	return countPairElem;
+}
+
+int main()
+{
+	long long number;
+	std::cin >> number;
+    std::cout << countPair(number);
+	return 0;
+}
+
