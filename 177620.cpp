63a56cc0e40d70f31f2ad1239035e13e9a12a56c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-18 16:33:24 +0300

    problem 177620

---
+++ 177620.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int countNotEven() {
+	int count = 0;
+	int number;
+	while (std::cin >> number) {
+		if (number == 0) {
+			break;
+		}
+		if (number % 2 != 0) {
+			++count;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+    std::cout << countNotEven();
+	return 0;
+}
+
