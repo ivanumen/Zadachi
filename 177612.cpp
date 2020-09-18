a79f899ca46c0ddf786f38cea676473dc3740461
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-18 16:52:45 +0300

    problem 177612

---
+++ 177612.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using  namespace std;
+
+vector <int> marathon(const int prevNumber, const int nextNumber) {
+	vector <int> in;
+	for (int i = nextNumber; i >= prevNumber; i--) {
+		in.push_back(i);
+	}
+	return in;
+}
+
+
+template <class T>
+void writeVector(const vector <T>& out) {
+	for (const auto& elem : out) {
+		cout << elem << " ";
+	}
+}
+
+int main()
+{
+	int prevNumber, nextNumber;
+	cin >> prevNumber >> nextNumber;
+	writeVector(marathon(prevNumber, nextNumber));
+	return 0;
+}
+
