7150bc97e86c8e498fc1c2cd1746816591a953e2
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-18 20:31:36 +0300

    problem 177616

---
+++ 177616.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+void createWhitespaceMatrix(vector <vector <T> >& in, const int rows) {
+	in.resize(rows);
+	for (auto& v : in) {
+		v.resize(rows);
+		for (auto& elem : v) {
+			elem = ' ';
+		}
+	}
+}
+
+template <class T>
+void createStarsVectorVector(vector <vector <T> >& in, const int rows) {
+	int size = rows - 1;
+	for (int i = 0; i < rows; i++) {
+		in[i][i] = '*';
+		in[size][i] = '*';
+		size--;
+	}
+}
+
+template <class T>
+void writeVectorVector(const vector <vector <T> >& out) {
+	for (const auto& v : out) {
+		for (const auto& elem : v) {
+			cout << elem << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	vector <vector <char> > in;
+	int rows;
+	cin >> rows;
+	createWhitespaceMatrix(in, rows);
+	createStarsVectorVector(in, rows);
+	writeVectorVector(in);
+	return 0;
+}
+
