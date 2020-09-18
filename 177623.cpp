27d57889748cdc5f17085f0757babb6d3224936c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-18 16:23:52 +0300

    problem 177623

---
+++ 177623.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+string twoConditions(const int number) {
+	if (number % 3 == 0 && number % 2 == 0 && (number >= 10 && number < 100 || number <= -10 && number > -100)) {
+		return "YES";
+	}
+	return "NO";
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	cout << twoConditions(number);
+	return 0;
+}
+
