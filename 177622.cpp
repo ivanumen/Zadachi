93ea4e995a0797a73096e6a66131f7d8f5109e5f
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-18 23:54:14 +0300

    problem 177622

---
+++ 177622.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void indexSpaceInLine(const string line) {
+	int indexSpaceStart = 0;
+	int indexSpaceFinish = 0;
+	bool start = true;
+	for (int i = 0; i < line.size(); i++) {
+		if (line[i] == ' ') {
+			if (start) {
+				indexSpaceStart = i;
+				start = false;
+			}
+			indexSpaceFinish = i;
+		}
+	}
+	if (indexSpaceFinish == 0) {
+		cout << -1;
+	}
+	else {
+		cout << indexSpaceStart << " " << indexSpaceFinish;
+	}
+}
+
+
+
+int main()
+{
+	string line;
+	getline(cin, line);
+	indexSpaceInLine(line);
+	return 0;
+}
+
