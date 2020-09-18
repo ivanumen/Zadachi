1b5bb08b4149400cac01dcaa3b88def75361a740
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-18 15:37:46 +0300

    problem 177609

---
+++ 177609.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+long long prevNumberEven(long long number) {
+	if ((number % 2) == 0) {
+		return number - 2;
+	}
+	else {
+		return --number;
+	}
+}
+
+int main()
+{
+	long long number;
+	std::cin >> number;
+    std::cout << prevNumberEven(number);
+	return 0;
+}
