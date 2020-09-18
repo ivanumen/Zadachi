f57f8337fc3302a47e4213bee23b04f0cc65681b
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-18 20:41:17 +0300

    problem 177624

---
+++ 177624.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVector(vector <T>& in) {
+	int number;
+	cin >> number;
+	in.resize(number);
+	for (auto& elem : in) {
+		cin >> elem;
+	}
+}
+
+template <class T>
+T maxElemVector(const vector <T>& in) {
+	return *max_element(in.begin(), in.end());
+}
+
+template <class T>
+T minElemVector(const vector <T>& in) {
+	return *min_element(in.begin(), in.end());
+}
+
+template <class T>
+T sumVectorNotMinAndMaxElem(const vector <T>& in) {
+	T ans = 0;
+	for (const auto elem : in) {
+		if (elem != minElemVector(in) && elem != maxElemVector(in)) {
+			ans += elem;
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	vector <int> in;
+	readVector(in);
+    cout << sumVectorNotMinAndMaxElem(in);
+	return 0;
+}
