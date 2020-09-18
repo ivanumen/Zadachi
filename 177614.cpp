713c00450821154720431e1607db8a48a8530c31
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-18 15:23:58 +0300

    problem 177614

---
+++ 177614.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+    cout << min(min(a, b), min(a,c)) << " " << min(max(a, b), min(max(a, c), max(b, c))) 
+		<< " " << max(max(a, b), max(a, c));
+	return 0;
+}
+
