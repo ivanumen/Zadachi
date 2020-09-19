a5e09015c0b7e7e69244228f38cf86ab157883cf
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-19 13:32:24 +0300

    problem 177617

---
+++ 177617.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string lineNotA(const string line) {
+	string lineNot = "";
+	for (int i = 0; i < line.size(); i++) {
+		if (line[i] != 'a') {
+			lineNot += line[i];
+		}
+	}
+	return lineNot;
+}
+
+int main()
+{
+	string line;
+	getline(cin, line);
+    cout << lineNotA(line);
+	return 0;
+}
+
