0e4e4f541e0fc187ad9934281c4175496fd3a0df
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-19 13:50:31 +0300

    problem 177627

---
+++ 177627.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T>
+bool vowelLetter(const T line) {
+	bool VowelLetter = false;
+	if (line == 'a' || line == 'e' || line == 'i' || line == 'o' || line == 'u' || line == 'y' ||
+		line == 'A' || line == 'E' || line == 'I' || line == 'O' || line == 'U' || line == 'Y') {
+		VowelLetter = true;
+	}
+	return VowelLetter;
+}
+
+string lineNotA(const string line) {
+	string lineNot = "";
+	for (int i = 0; i < line.size(); i++) {
+		if (line[i] == 'a') {
+			lineNot += line[i];
+		}
+		lineNot += line[i];
+	}
+	return lineNot;
+}
+
+int main()
+{
+	string line;
+	getline(cin, line);
+	cout << lineNotA(line);
+	return 0;
+}
