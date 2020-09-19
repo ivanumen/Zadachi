5cb7f6a7163cba13ee0a6cbb3a0da6ba95208d4a
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-19 13:08:29 +0300

    problem 177626

---
+++ 177626.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int journey() {
+	int countCities, onFoot, teleportation;
+	int countFatigue = 0;
+	vector <int> cities;
+	cin >> countCities >> onFoot >> teleportation;
+	cities.resize(countCities);
+	for (auto& elem : cities) {
+		cin >> elem;
+	}
+	for (int i = 0; i < countCities - 1; i++) {
+		if ((cities[i + 1] - cities[i]) * onFoot < teleportation) {
+			countFatigue += (cities[i + 1] - cities[i]) * onFoot;
+		}
+		else {
+			countFatigue += teleportation;
+		}
+	}
+	return countFatigue;
+}
+
+int main()
+{
+    cout << journey();
+}
+
