8dc83ab522644c5b72c887b679abe485ee2ff8b0
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-20 17:59:03 +0300

    problem 178528

---
+++ 178528.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+vector <int> numericalDensity(const string& number) {
+	vector <int> density = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int index;
+	for (int i = 0; i < number.size(); i++) {
+		index = number[i] - '0';
+		++density[index];
+	}
+	return density;
+}
+
+template <class T>
+void writeVector(const vector <T>& out) {
+	for (const auto& elem : out) {
+		cout << elem << " ";
+	}
+}
+
+int main()
+{
+	string number;
+	cin >> number;
+	writeVector(numericalDensity(number));
+	return 0;
+}
+
