4da13766ba75be91472c9f4158ee2ba31b97d9ff
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-20 14:33:01 +0300

    problem 178526

---
+++ 178526.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+string lineNotOdd(const string line) {
+	string lineNot = "";
+	int digit;
+	for (int i = 0; i < line.size(); i++) {
+		if (isdigit(line[i])) {
+			digit = line[i] - '0';
+			if (digit % 2 > 0) {
+				continue;
+			}
+		}
+		lineNot += line[i];
+	}
+	return lineNot;
+}
+
+int main()
+{
+	string line;
+	getline(cin, line);
+	cout << lineNotOdd(line);
+	return 0;
+}
