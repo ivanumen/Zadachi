d4e906408251ac9900f85a53acdc29e91985208c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-22 21:53:30 +0300

    problem 178535

---
+++ 178535.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+long long sumIndexNumber(const long long number) {
+	long long sumIndex = 0;
+	for (long long i = 0; i <= number; i++) {
+		sumIndex += i;
+	}
+	return sumIndex;
+}
+
+int main()
+{
+	long long number;
+	std::cin >> number;
+    std::cout << sumIndexNumber(number) + number - 1;
+	return 0;
+}
+
