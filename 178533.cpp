e90961cc0f6bca7c435ed3ce59eab4af65f52afb
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-22 22:58:44 +0300

    problem 178533

---
+++ 178533.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void writeMinAndMaxFractions(const long long a, const long long b, const long long c, const long long d) {
+	if ((a * d) > (c * b)) {
+		std::cout << c << "/" << d << " " << a << "/" << b;
+	}
+	else {
+		std::cout << a << "/" << b << " " << c << "/" << d;
+	}
+}
+
+int main()
+{
+	long long a, b, c, d;
+	char sym;
+	std::cin >> a >> sym >> b >> c >> sym >> d;
+	writeMinAndMaxFractions(a, b, c, d);
+	return 0;
+}
+
