4d58c136419cf80b0750317be1b15e806eb94d1c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-23 22:09:25 +0300

    problem 178537

---
+++ 178537.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+void readVectorVector(vector <vector <T> >& in, const int rows, const int cols) {
+	in.resize(rows);
+	for (auto& v : in) {
+		v.resize(cols);
+		for (auto& elem : v) {
+			cin >> elem;
+		}
+	}
+}
+
+int numberOfDiseasedCells(const vector <vector <char> >& in, const int rows, const int cols) {
+	int countDiseasedCells = 0;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if ((j < cols - 1) && (in[i][j] == '.' && in[i][j + 1] == '.')) {
+				++countDiseasedCells;
+			}
+			if ((i < rows - 1) && (in[i][j] == '.' && in[i + 1][j] == '.')) {
+				++countDiseasedCells;
+			}
+		}
+	}
+	return countDiseasedCells;
+}
+
+
+template <class T>
+void writeVectorVector(const vector <vector <T> >& out) {
+	for (const auto& v : out) {
+		for (const auto& elem : v) {
+			cout << elem << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	vector <vector <char> > in;
+	int rows, cols;
+	cin >> rows >> cols;
+	readVectorVector(in, rows, cols);
+	cout << numberOfDiseasedCells(in, rows, cols);
+	return 0;
+}
