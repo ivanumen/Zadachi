8c0e4fa4225fd47a7967b9e17b3229cc0ccf424b
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-09-28 15:39:47 +0300

    problem 178531

---
+++ 178531.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int numberOfDigits(string line) {
+	set <string> lines;
+	string linej;
+	int size = 1;
+	for (int i = 0; i < line.size(); i++) {
+		for (int j = 0; j < line.size(); j++) {
+			linej = line.substr(j, size);
+			lines.insert(linej);
+		}
+		size++;
+	}
+	return lines.size();
+}
+
+int main()
+{
+	string line;
+	cin >> line;
+	cout << numberOfDigits(line);
+	return 0;
+}
+
