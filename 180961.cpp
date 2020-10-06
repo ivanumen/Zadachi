22d0f3904e9d4c8d0c6b004f483258b347283416
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-06 17:09:43 +0300

    problem 180961

---
+++ 180961.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+template <class T>
+void readSet(set <T>& in) {
+	int number, x;
+	cin >> number;
+	for (int i = 0; i < number; i++) {
+		cin >> x;
+		x = abs(x);
+		in.emplace(x);
+	}
+}
+
+
+
+int main()
+{
+	set <int> elements;
+	readSet(elements);
+    cout << elements.size();
+	return 0;
+}
