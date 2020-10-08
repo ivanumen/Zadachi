548cdf66fb07467e8f1fd91ecc38003106d9bb79
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-08 21:22:17 +0300

    problem 180963

---
+++ 180963.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int middleElement(const long long number1, const long long number2, const long long number3) {
+	if (number1 == number2 || number1 == number3 || number2 == number3) {
+		return 0;
+	}
+	if ((number1 > number2 && number1 < number3) || (number1 < number2 && number1 > number3)) {
+		return 1;
+	}
+	if ((number2 > number1 && number2 < number3) || (number2 < number1 && number2 > number3)) {
+		return 2;
+	}
+	return 3;
+}
+
+int main()
+{
+	long long number1, number2, number3;
+	cin >> number1 >> number2 >> number3;
+	cout << middleElement(number1, number2, number3);
+	return 0;
+}
+
+
