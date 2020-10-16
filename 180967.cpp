41fbafa541022a2ffc04cdf00770e7947ce4372b
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-16 15:44:59 +0300

    problem 183578

---
+++ 180967.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVectorVector(vector <vector <T> >& in) {
+	int rows, cols;
+	cin >> rows >> cols;
+	in.resize(rows);
+	for (auto& v : in) {
+		v.resize(cols);
+		for (auto& elem : v) {
+			cin >> elem;
+		}
+	}
+}
+
+int sumSubarray(const vector <vector <int> >& in, const int leftRow, const int leftcol, const int rightRow, const int rightCol) {
+	int ans = 0;
+	for (int i = leftRow; i <= rightRow; i++) {
+		for (int j = leftcol; j <= rightCol; j++) {
+			ans += in[i][j];
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	vector <vector <int> > in;
+	int leftRow, leftcol, rightRow, rightCol;
+	readVectorVector(in);
+	cin >> leftRow >> leftcol >> rightRow >> rightCol;
+    cout << sumSubarray(in, leftRow - 1, leftcol - 1, rightRow - 1, rightCol - 1);
+	return 0;
+}
+
---
+++ 183578.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+vector <int> intToVector(T number) {
+	vector <T> decomposition;
+	int secondNumber;
+	while (number > 0) {
+		secondNumber = number % 10;
+		number /= 10;
+		decomposition.push_back(secondNumber);
+	}
+	reverse(decomposition.begin(), decomposition.end());
+	return decomposition;
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+	vector <int> in = intToVector(number);
+	cout << in[in.size() - 3];
+	return 0;
+}
+
