774dc8e8d7f8c1d53b8d04e1d522c0db0e8359fb
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-16 17:10:32 +0300

    problem 183581

---
+++ 183581.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string threeDigitNumber(const int number) {
+	if ((number < 1000 && number > 99) || (-number < 1000 && -number > 99)) {
+		return "Ok";
+	}
+	else {
+		return "No";
+	}
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+    cout << threeDigitNumber(number);
+	return 0;
+}
