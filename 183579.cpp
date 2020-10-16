eb99adb605f05e47d5a8ef22d3b7390874f95210
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-16 17:02:51 +0300

    problem 183579

---
+++ 183579.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+template <class T>
+T binPow(T value, int extent) {
+	T res = 1;
+	while (extent > 0) {
+		if (extent % 2 == 1) {
+			--extent;
+			res *= value;
+		}
+		else {
+			extent /= 2;
+			value *= value;
+		}
+	}
+	return res;
+}
+
+int main()
+{
+	int extent;
+	long long value = 5;
+	cin >> extent;
+	cout << binPow(value, extent);
+	return 0;
+}
