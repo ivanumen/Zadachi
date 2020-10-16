1c0eb7883c50ae6b2f55ca80078aaba94f3a6f1c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-16 15:51:40 +0300

    problem 183577

---
+++ 183577.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int sameSigns(const int number1, const int number2) {
+	if ((number1 < 0 && number2 < 0) || (number1 > 0 && number2 > 0)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int main()
+{
+	int number1, number2;
+	std::cin >> number1 >> number2;
+    std::cout << sameSigns(number1, number2);
+	return 0;
+}
+
