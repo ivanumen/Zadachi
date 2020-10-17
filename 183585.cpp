4e7bf1346e8c4fb106f8af7c9583efcfdc45dc2a
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-17 19:55:00 +0300

    problem 183585

---
+++ 183585.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVector(vector <T>& in) {
+	int number;
+	cin >> number;
+	in.resize(number);
+	for (auto& elem : in) {
+		cin >> elem;
+	}
+}
+
+template <class T>
+void writeVector(const vector <T>& out) {
+	for (const auto& elem : out) {
+		cout << elem << " ";
+	}
+}
+
+vector <int> initPrevElem(vector <int>& in) {
+	vector <int> out;
+	out.push_back(in[0]);
+	for (int i = 1; i < in.size(); i++) {
+		out.push_back(in[i] - in[i - 1]);
+	}
+	return out;
+}
+
+int main()
+{
+	vector <int> in;
+	readVector(in);
+	writeVector(initPrevElem(in));
+	return 0;
+}
+
