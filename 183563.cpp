726d4caec61c5adc8e03dde13ef8e92042794771
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-17 19:42:12 +0300

    problem 183563

---
+++ 183563.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+void writeVectorVector(const vector <vector <T> >& out) {
+	for (const auto& v : out) {
+		for (const auto& elem : v) {
+			cout << elem;
+		}
+		cout << endl;
+	}
+}
+
+void createArrayStars(vector <vector <char> >& in) {
+	int size;
+	cin >> size;
+	int countStar = size;
+	in.resize(size);
+	for (auto& v : in) {
+		v.resize(countStar);
+		for (auto& elem : v) {
+			elem = '*';
+		}
+		--countStar;
+	}
+}
+
+int main()
+{
+	vector <vector <char> > in;
+	createArrayStars(in);
+	writeVectorVector(in);
+	return 0;
+}
