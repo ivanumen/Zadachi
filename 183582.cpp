b4973b0b3ffedd72096b6b4d5a3cf517d0e7e808
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-17 13:33:46 +0300

    problem 183582

---
+++ 183582.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string replacingCharacters(const string& inLeng) {
+	string outLeng = "";
+	for (int i = 0; i < inLeng.size(); i++) {
+		if (inLeng[i] == 'a' && inLeng[i + 1] == 'b' && i < inLeng.size() - 1) {
+			outLeng += "ups";
+			++i;
+		}
+		else {
+			outLeng += inLeng[i];
+		}
+	}
+	return outLeng;
+}
+
+int main()
+{
+	string leng;
+	getline(cin, leng);
+	cout << replacingCharacters(leng);
+	return 0;
+}
+
