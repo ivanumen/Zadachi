28d7f50a2e5ca5de2fcbf57846e6052c07fafb8c
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-23 18:21:11 +0300

    problem 183592

---
+++ 183592.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVector(vector <T>& in, int number) {
+	in.resize(number);
+	for (auto& elem : in) {
+		cin >> elem;
+	}
+}
+
+template <class T>
+T maxElemVector(const vector <T>& in) {
+	return *max_element(in.begin(), in.end());
+}
+
+template <class T>
+T minElemVector(const vector <T>& in) {
+	return *min_element(in.begin(), in.end());
+}
+
+int countMushroom(vector <int>& in, const int number) {
+	int count = 0;
+	while (1) {
+		++count;
+		if (count > minElemVector(in)) {
+			return -1;
+		}
+		if ((count * number) > (maxElemVector(in) - count)) {
+			return count;
+		}
+
+	}
+}
+
+int main()
+{
+	vector <int> in;
+	int number;
+	cin >> number;
+	readVector(in, number - 1);
+	cout << countMushroom(in, number - 1);
+	return 0;
+}
+
