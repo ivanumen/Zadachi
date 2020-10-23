274fbdee4ab206d1d432474e4d5562e387659af8
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-23 21:20:29 +0300

    problem 183589

---
+++ 183589.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+long long powTeen(long long number) {
+	long long pow = 0;
+	while (number > 1) {
+		++pow;
+		if (number % 10 != 0) {
+			return 0;
+		}
+		number /= 10;
+	}
+	return pow;
+}
+
+int main()
+{
+	long long number;
+	std::cin >> number;
+	if (powTeen(number) == 0 && number != 1) {
+		std::cout << "No";
+	}
+	else {
+		std::cout << powTeen(number);
+	}
+	return 0;
+}
