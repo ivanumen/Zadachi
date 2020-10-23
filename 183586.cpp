d2828b4acad25e847606adc68f86387ce7d2ad27
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-23 17:44:07 +0300

    problem 183586

---
+++ 183586.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVector(vector <T>& in, int number) {
+	in.resize(number);
+	for (auto& elem : in) {
+		cin >> elem;
+	}
+}
+
+int main()
+{
+	int number, countSearch, searchNumber;
+	vector <int> in;
+	cin >> number >> countSearch;
+	readVector(in, number);
+	for (int i = 0; i < countSearch; i++) {
+		cin >> searchNumber;
+		if (binary_search(in.begin(), in.end(), searchNumber)) {
+			cout << "YES" << endl;
+		}
+		else {
+			cout << "No" << endl;
+		}
+	}
+	return 0;
+}
---
+++ 183591.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+pair <int, int> sumPointAndNumber(const int pointA, const int pointB, const int pointC, const int pointD, const int number) {
+	return make_pair(pointA + pointC + number, pointB + pointD + number);
+}
+
+template <class T>
+void writePair(const pair<T, T>& in) {
+	cout << in.first << " " << in.second;
+}
+
+int main()
+{
+	int pointA, pointB, pointC, pointD, number;
+	cin >> pointA >> pointB >> pointC >> pointD >> number;
+	writePair(sumPointAndNumber(pointA, pointB, pointC, pointD, number));
+	return 0;
+}
+
+
