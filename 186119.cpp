3772d286cd97fc0f8d034889267f0a091275e080
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-25 22:25:35 +0300

    problem 186119

---
+++ 186119.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void notMultiplesOfTwoThreeOrFive(int number) {
+	int count = 0;
+	while (number != 0) {
+		++count;
+		if (count % 2 != 0 && count % 3 != 0 && count % 5 != 0) {
+			--number;
+			std::cout << count << " ";
+		}
+	}
+}
+
+int main()
+{
+	int number;
+	std::cin >> number;
+	notMultiplesOfTwoThreeOrFive(number);
+	return 0;
+}
+
