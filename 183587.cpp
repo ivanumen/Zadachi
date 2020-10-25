c22aab988efe4fc4c33d1586b26cec8e7668d5ed
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-10-25 20:00:54 +0300

    problem 1861177

---
+++ 183587.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVector(vector <T>& in, int number) {
+	in.resize(number);
+	for (auto& elem : in) {
+		cin >> elem;
+	}
+}
+
+int splittingIntoSubarrays(const vector <int>& in, int subarrays, const int number) {
+	int sumSubarray = 0;
+	for (int i = 0; i < in.size(); i++) {
+		sumSubarray += in[i];
+		if (sumSubarray > number) {
+			sumSubarray = in[i];
+			--subarrays;
+		}
+		if (subarrays <= 0 || in[i] > number) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+template <class T>
+T sumVector(const vector <T>& in) {
+	T ans = 0;
+	for (const auto elem : in) {
+		ans += elem;
+	}
+	return ans;
+}
+
+int binarySearch(const vector <int>& in, const int subarrays) {
+	int ans; 
+	int left = 0;
+	int right = abs(sumVector(in));
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (splittingIntoSubarrays(in, subarrays, mid) == 1) {
+			ans = mid;
+			right = mid - 1;
+		}
+		else {
+			left = mid + 1;
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	vector <int> in;
+	int subarrays, number;
+	cin >> number >> subarrays;
+	readVector(in, number);
+    cout << binarySearch(in, subarrays);
+	return 0;
+}
---
+++ 1861177.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void oddNumber(const int numberFirst, const int numberEnd) {
+	for (int i = numberFirst; i <= numberEnd; i++) {
+		if ((i / 1000) % 2 == 0) {
+			i -= (i % 1000);
+			i += 1000;
+		}
+		if (((i / 100) % 10) % 2 == 0) {
+			i -= (i % 100);
+			i += 100;
+		}
+		if (((i / 10) % 10) % 2 == 0) {
+			i -= (i % 10);
+			i += 10;
+		}
+		if ((i % 10) % 2 == 0) {
+			i += 1;
+		}
+		if (i <= numberEnd) {
+			cout << i << " ";
+		}
+	}
+}
+
+int main()
+{
+	int numberFirst, numberEnd;
+	cin >> numberFirst >> numberEnd;
+	oddNumber(numberFirst, numberEnd);
+	return 0;
+}
