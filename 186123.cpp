5d1addad2e9e31b1f515d71b37eb06c5249d6ad8
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-02 19:06:22 +0300

    problem 186123

---
+++ 186123.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVector(vector <T>& in) {
+	int number;
+	cin >> number;
+	in.resize(number);
+	for (auto& elem : in) {
+		cin >> elem;
+	}
+}
+
+template <class T>
+T maxElemVector(const vector <T>& in) {
+	return *max_element(in.begin(), in.end());
+}
+
+template <class T>
+T minElemVector(const vector <T>& in) {
+	return *min_element(in.begin(), in.end());
+}
+
+void writePrimordialVector(vector <int>& in) {
+	int max = maxElemVector(in);
+	int min = minElemVector(in);
+	for (const auto& elem : in) {
+		if (elem == 0) {
+			cout << elem << " ";
+			continue;
+		}
+		if (elem > 0) {
+			cout << elem - (max / 2) << " ";
+		}
+		else {
+			cout << elem - (min / 2) << " ";
+		}
+	}
+}
+
+
+int main()
+{
+	vector <int> in;
+	readVector(in);
+	writePrimordialVector(in);
+	return 0;
+}
+
