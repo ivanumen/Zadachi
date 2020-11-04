648d2319c57a0cf05a32681af4bb922a796ee362
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-04 22:47:05 +0300

    problem 18613000

---
+++ 18613000.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int countDividers(const int number) {
+	int count = 0;
+	for (int i = 2; (i * i) <= number; i++) {
+		if ((i * i) == number) {
+			++count;
+			continue;
+		}
+		if (number % i == 0) {
+			count += 2;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+	int number;
+	std::cin >> number;
+	std::cout << countDividers(number);
+	return 0;
+}
+
+
