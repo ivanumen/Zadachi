7891cd0e5218581d4017e59196f569a58a5b30b3
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-05 17:29:00 +0300

    problem 186133

---
+++ 186133.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int number, gynka, myska, knopka;
+	cin >> number;
+	if (number % 2 == 0) {
+		myska = 0.3 * number;
+		knopka = myska;
+	}
+	else {
+		myska = (0.3 * number) + 1;
+		knopka = myska - 1;
+	}
+	gynka = (myska + knopka) / 1.5;
+    cout << gynka  << " " << myska << " " << knopka;
+	return 0;
+}
+
