cea5eb75748a11239dce1d31e4ab6c105e8b64af
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-11 20:29:26 +0300

    problem 190854

---
+++ 190854.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int divisibility(const int number1, const int number2) {
+	return number1 % number2 == 0 ? 1 : 0;
+}
+
+int main()
+{
+	int number1, number2;
+	std::cin >> number1 >> number2;
+    std::cout << divisibility(number1, number2);
+	return 0;
+}
+
+
