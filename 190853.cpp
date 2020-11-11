dcefc264372e381db2b7ba2b0d8457dac8bdb29b
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-11 19:29:44 +0300

    problem 190853

---
+++ 190853.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	cin >> number;
+	if (sqrt(number) == int(sqrt(number))) {
+		cout << sqrt(number);
+	}
+	else {
+		cout << "No";
+	}
+	return 0;
+}
+
