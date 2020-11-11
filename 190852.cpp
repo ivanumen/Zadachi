d7dfb32a1ba94738a9a36a5940352cbecb030210
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-11 18:50:22 +0300

    problem 190852

---
+++ 190852.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int prevOddNumber(int number) {
+	if ((number % 2) == 0) {
+		return --number;
+	}
+	return number - 2;
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+    cout << prevOddNumber(number);
+	return 0;
+}
+
