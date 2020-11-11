758dccf57d1eb97c98ec7800e8a9c29b1d461804
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-11 18:34:17 +0300

    problem 189213

---
+++ 189213.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <algorithm>
+
+
+int main()
+{
+	int number1, number2, number3;
+	std::cin >> number1 >> number2 >> number3;
+    std::cout << std::min(std::min(number1, number2), std::min(number1, number3));
+	return 0;
+}
+
