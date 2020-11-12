3e0846b781ee630efb9964cc118c79f0aa206b68
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-12 16:34:51 +0300

    problem 190856

---
+++ 190856.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+	int number;
+	std::cin >> number;
+    std::cout << ((((number / 100) * 10) + (number % 10)) * 10) + ((number % 100) / 10);
+	return 0;
+}
+
+
