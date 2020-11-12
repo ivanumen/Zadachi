dc90f3162f30ab6578139aac407d6ccef1111a46
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-12 23:46:26 +0300

    problem 190857

---
+++ 190857.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVectorUntilEOF(vector <T>& in) {
+	T x;
+	while (cin >> x) {
+		if (x == 0) {
+			break;
+		}
+		in.push_back(x);
+	}
+}
+
+template <class T>
+T sumPositiveVector(const vector <T>& in) {
+	T ans = 0;
+	for (const auto elem : in) {
+		if (elem > 0) {
+			ans += elem;
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	vector <int> in;
+	readVectorUntilEOF(in);
+    cout << sumPositiveVector(in);
+	return 0;
+}
+
