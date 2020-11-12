5b228c37a4ccf69cea208f5d818e42ba1f198bd2
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-13 00:44:29 +0300

    problem 190859

---
+++ 190859.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+long long isoscelesTriangle(const long long sideA, const long long sideB, const long long sideC) {
+	return ((sideA + sideB > sideC && sideA + sideC > sideB && sideC + sideB > sideA) && (sideA == sideB || sideA == sideC || sideB == sideC)) ? (sideA + sideB + sideC) : 0;
+}
+
+int main()
+{
+	long long sideA, sideB, sideC;
+	std::cin >> sideA >> sideB >> sideC;
+	if (isoscelesTriangle(sideA, sideB, sideC) > 0) {
+		std::cout << isoscelesTriangle(sideA, sideB, sideC);
+	}
+	else {
+		std::cout << "No";
+	}
+	return 0;
+}
+
+
