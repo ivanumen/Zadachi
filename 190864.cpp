408793ee3e68082773371ab73f3b8e6c10ac831f
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-17 16:38:30 +0300

    problem 190864

---
+++ 190864.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool arifmaticalOperation(const char in) {
+	return in == '+' || in == '-' || in == '/' || in == '*' || in == '%' ? true : false;
+}
+
+int countArifmaticalOperation(const string& in) {
+	int count = 0;
+	for (int i = 0; i < in.size(); i++) {
+		if (arifmaticalOperation(in[i])) {
+			++count;
+			++i;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+	string in;
+	cin >> in;
+	cout << countArifmaticalOperation(in);
+	return 0;
+}
+
+
