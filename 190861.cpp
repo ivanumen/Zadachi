7412cdbb4d726f0f909997dce601e56b2070c510
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-17 15:38:32 +0300

    problem 19061

---
+++ 190861.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+long long get(const long long elem, const long long number) {
+	return elem / number;
+}
+
+long long divisibilityInTheInterval(const long long start, const long long end, const long long number) {
+	return start == 0 ? get(end, number) + 1 : get(end, number) - get((start - 1), number);
+}
+
+int main()
+{
+	long long start, end, number;
+	std::cin >> start >> end >> number;
+	std::cout << divisibilityInTheInterval(start, end, number);
+	return 0;
+}
+
+
