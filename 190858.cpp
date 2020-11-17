5e8129598758106b711d6e1ccf44f38348c666eb
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-17 16:08:43 +0300

    problem 190858

---
+++ 190858.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <class T>
+void readVectorVector(vector <vector <T> >& in, const int rows, const int cols) {
+	in.resize(rows);
+	for (auto& v : in) {
+		v.resize(cols);
+		for (auto& elem : v) {
+			cin >> elem;
+		}
+	}
+}
+
+template <class T>
+void writeVectorVector(const vector <vector <T> >& out) {
+	for (const auto& v : out) {
+		for (const auto& elem : v) {
+			cout << elem << " ";
+		}
+		cout << endl;
+	}
+}
+
+vector <vector <int> > tranformVectorVector(const vector <vector <int> >& in, const int rows, const int cols) {
+	vector <vector <int> > out(cols, vector <int>(rows));
+	for (int i = 0; i < cols; i++) {
+		for (int j = 0; j < rows; j++) {
+			out[i][j] = in[j][i];
+		}
+	}
+	return out;
+}
+
+int main()
+{
+	vector <vector <int> > in;
+	int rows, cols;
+	cin >> rows >> cols;
+	readVectorVector(in, rows, cols);
+	writeVectorVector(tranformVectorVector(in, rows, cols));
+	return 0;
+}
+
+
