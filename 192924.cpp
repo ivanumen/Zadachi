475f6c756f49bbc12f4fe6102ee5b9023bd3e0e7
ivanumen <60342088+ivanumen@users.noreply.github.com>
2020-11-21 18:54:02 +0300

    problem 192924

---
+++ 192924.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int theSmallestOfTheBigOnes(const int number) {
+	int bigNumber = number + 1;
+	while (true)
+	{
+		if (bigNumber % 2 == 0 || bigNumber % 3 == 0 || bigNumber % 5 == 0) {
+			++bigNumber;
+			continue;
+		}
+		return bigNumber;
+	}
+}
+
+int main()
+{
+	int number;
+	std::cin >> number;
+    std::cout << theSmallestOfTheBigOnes(number);
+}
