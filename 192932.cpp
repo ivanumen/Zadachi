09bd159f2f31dc00a18231a18c86eb48dcb65781
ivanumen <ivan.umen@mail.ru>
2020-12-04 21:44:29 +0300

    Problem 192932

---
+++ 192932.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string searchWord(string& inWord, const string& outWord) {
+    vector <int> in(1000);
+    for (int i = 0; i < inWord.size(); i++) {
+        ++in[inWord[i]];
+    }
+    for (int i = 0; i < outWord.size(); i++) {
+        --in[outWord[i]];
+        if (in[outWord[i]] < 0) {
+            return "No";
+        }
+    }
+    return "Ok";
+}
+
+int main()
+{
+    string inWord, outWord;
+    getline(cin, inWord);
+    getline(cin, outWord);
+    cout << searchWord(inWord, outWord);
+    return 0;
+}
+
