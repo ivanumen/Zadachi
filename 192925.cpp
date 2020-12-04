f501f182f037ee29e98c70d33c9c07dc8fdb57ac
ivanumen <ivan.umen@mail.ru>
2020-12-04 18:58:21 +0300

    Problem 192925

---
+++ 192925.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int readAddSubtractMultiplyNumber() {
+    int number = 0;
+    int operationNumber;
+    string operation;
+    while (cin >> operation) {
+        cin >> operationNumber;
+        if (operation == "add") {
+            number += operationNumber;
+        }
+        if (operation == "subtract") {
+            number -= operationNumber;
+        }
+        if (operation == "multiply") {
+            number *= operationNumber;
+        }
+    }
+    return number;
+}
+
+int main()
+{
+    cout << readAddSubtractMultiplyNumber();
+    return 0;
+}
+
